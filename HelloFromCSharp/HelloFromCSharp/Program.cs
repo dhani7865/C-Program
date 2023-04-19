09f93807a072fcb0ca086e970914ff2cbe1b7185
dhanyaal <dh11any@hotmail.com>
2023-04-19 14:05:13 +0100

    c# program

---
+++ HelloFromCSharp/HelloFromCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloFromCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everybody!");
+
+            Console.WriteLine("Please enter your Firstname: ");
+            String FirstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your lastName: ");
+            String lastName = Console.ReadLine();
+
+            // Console.WriteLine("Hello " + name);
+            Console.WriteLine("Hello " + FirstName + " " + lastName);
+
+        }
+    }
+}
---
+++ HelloWorldFromCLI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorldFromCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everybody!");
+
+            Console.WriteLine("Please enter your Firstname: ");
+            String FirstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your lastName: ");
+            String lastName = Console.ReadLine();
+
+            // Console.WriteLine("Hello " + name);
+            Console.WriteLine("Hello " + FirstName + " " + lastName);        }
+    }
+}
